0423cfabbcdd37511c426dbb611fcf77a61669a9
Jakob Schöttl <jschoett@gmail.com>
2012-01-06 13:14:06 +0100

    Ticket set added.

--- ScotlYard/src/kj/scotlyard/player/Player.java
+++ ScotlYard/src/kj/scotlyard/player/Player.java
@@ -1,16 +1,21 @@
 package kj.scotlyard.player;
 
 import kj.scotlyard.game.Move;
+import kj.scotlyard.game.ticket.Ticket;
+import kj.set.ObjectSet;
 
 
 public abstract class Player {
 
 	private String name;
 	
+	private ObjectSet<Ticket> tickets;
+	
 	private Move move;
 	
 	protected Player(String name) { // protected oder public? bei protected waere die Klasse auch ohne abstract nicht instanziierbar. 
 		this.name = name;
+		this.tickets = new ObjectSet<>();
 	}
 	
 	public String getName() {
@@ -20,12 +25,21 @@ public abstract class Player {
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	/**
+	 * Menge der Tickets dieses Spielers.
+	 * @return
+	 */
+	public ObjectSet<Ticket> getTickets() {
+		return tickets;
+	}
 
 	/**
 	 * Entscheidet, welchen Zug der Player machen wird.
 	 * Entspricht dem Setter fuer move.
 	 * 
 	 * Wird vom richtigen Spieler ueber GUI oder von der AI aufgerufen.
+	 * Sobald festgelegt, ist der Player fertig (<tt>isReady()</tt>).
 	 * 
 	 * @param move der geplante nacheste Spielzug
 	 */
@@ -37,8 +51,8 @@ public abstract class Player {
 	 * Player fuehrt den festgelegten Zug aus.
 	 * Entspricht dem Getter fuer move.
 	 * 
-	 * Wird vom GameController abgefragt, wenn
-	 * Player an der Reihe und fertig ist.
+	 * Wird vom GameController abgefragt, wenn Player
+	 * an der Reihe und fertig (<tt>isReady()</tt>) ist.
 	 * 
 	 * Wird nach dem Abfragen zurueckgesetzt,
 	 * da jeder Spielzug nur einmal ausgefuehrt
