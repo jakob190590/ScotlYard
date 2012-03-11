d3c4e1278a32db4d843d8f8cf0eb343053d51865
Jakob Schöttl <jschoett@gmail.com>
2012-03-11 23:29:49 +0100

    import clause changed

--- ScotlYard/src/kj/scotlyard/game/rules/GameInitPolicy.java
+++ ScotlYard/src/kj/scotlyard/game/rules/GameInitPolicy.java
@@ -3,8 +3,8 @@ package kj.scotlyard.game.rules;
 import java.util.Set;
 
 import kj.scotlyard.game.graph.StationVertex;
-import kj.scotlyard.game.items.Item;
 import kj.scotlyard.game.model.Player;
+import kj.scotlyard.game.model.items.Item;
 
 public interface GameInitPolicy {
 	
