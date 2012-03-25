d411e5ec40c8f693f419656f57a09003060d653f
Jakob Schöttl <jschoett@gmail.com>
2012-03-25 13:06:38 +0200

    Kleine Fehler in Policies beseitigt

--- ScotlYard/src/kj/scotlyard/game/rules/GameWinPolicy.java
+++ ScotlYard/src/kj/scotlyard/game/rules/GameWinPolicy.java
@@ -1,9 +1,10 @@
 package kj.scotlyard.game.rules;
 
+import kj.scotlyard.game.graph.GameGraph;
 import kj.scotlyard.game.model.GameState;
 
 public interface GameWinPolicy {
 
-	GameWin isGameWon(GameState gameState);
+	GameWin isGameWon(GameState gameState, GameGraph gameGraph);
 	
 }
--- ScotlYard/src/kj/scotlyard/game/rules/MovePolicy.java
+++ ScotlYard/src/kj/scotlyard/game/rules/MovePolicy.java
@@ -5,6 +5,7 @@ import kj.scotlyard.game.graph.GameGraph;
 import kj.scotlyard.game.model.GameState;
 import kj.scotlyard.game.model.Move;
 import kj.scotlyard.game.model.Player;
+import kj.scotlyard.game.model.items.Item;
 import kj.scotlyard.game.model.items.Ticket;
 
 public interface MovePolicy {	
@@ -13,6 +14,6 @@ public interface MovePolicy {
 	
 	void checkMove(GameState gameState, GameGraph gameGraph, Move move) throws IllegalMoveException;
 	
-	Player getNextItemOwner(GameState gameState, Move move);
+	Player getNextItemOwner(GameState gameState, Move move, Item item); // Parameter Item, falls es mal mehrere Items in einem Move gibt.
 	
 }
