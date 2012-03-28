96daee0fa1444b0d67330549079d64edd5202706
Jakob Schöttl <jschoett@gmail.com>
2012-03-28 02:14:44 +0200

    Test for all four Model Listener added

--- ScotlYard/test/kj/scotlyard/game/model/TheGameTest.java
+++ ScotlYard/test/kj/scotlyard/game/model/TheGameTest.java
@@ -20,6 +20,129 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class TheGameTest {
+	
+	class TestStateListener implements StateListener {
+
+		GameState gs;
+		
+		Player oldP, newP;
+		
+		int oldR, newR;
+		
+		@Override
+		public void currentPlayerChanged(GameState gameState, Player oldPlayer,
+				Player newPlayer) {
+			gs = gameState;
+			oldP = oldPlayer;
+			newP = newPlayer;
+			
+		}
+
+		@Override
+		public void currentRoundChanged(GameState gameState,
+				int oldRoundNumber, int newRoundNumber) {
+			gs = gameState;
+			oldR = oldRoundNumber;
+			newR = newRoundNumber;
+		}
+		
+	}
+	
+	class TestPlayerListener implements PlayerListener {
+
+		GameState gs;
+		
+		int i;
+		
+		DetectivePlayer d;
+		
+		MrXPlayer oldX, newX;
+		
+		@Override
+		public void detectiveAdded(GameState gameState,
+				DetectivePlayer detective, int atIndex) {
+			gs = gameState;
+			d = detective;
+			i = atIndex;			
+		}
+
+		@Override
+		public void detectiveRemoved(GameState gameState,
+				DetectivePlayer detective, int atIndex) {
+			gs = gameState;
+			d = detective;
+			i = atIndex;
+		}
+
+		@Override
+		public void mrXSet(GameState gameState, MrXPlayer oldMrX,
+				MrXPlayer newMrX) {
+			gs = gameState;
+			oldX = oldMrX;
+			newX = newMrX;
+		}
+		
+	}
+	
+	class TestItemListener implements ItemListener {
+
+		GameState gs;
+		
+		Item i;
+		
+		Player p;
+		
+		Set<Item> oldI, newI;
+		
+		@Override
+		public void itemAdded(GameState gameState, Player player, Item item) {
+			gs = gameState;
+			p = player;
+			i = item;
+		}
+
+		@Override
+		public void itemRemoved(GameState gameState, Player player, Item item) {
+			gs = gameState;
+			p = player;
+			i = item;
+		}
+
+		@Override
+		public void itemSetChanged(GameState gameState, Player player,
+				Set<Item> oldItems, Set<Item> newItems) {
+			gs = gameState;
+			p = player;
+			oldI = oldItems;
+			newI = newItems;
+		}
+		
+	}
+	
+	class TestMoveListener implements MoveListener {
+
+		GameState gs;
+		
+		Move m;
+		
+		@Override
+		public void moveDone(GameState gameState, Move move) {
+			gs = gameState;
+			m = move;			
+		}
+
+		@Override
+		public void moveUndone(GameState gameState, Move move) {
+			gs = gameState;
+			m = move;
+		}
+
+		@Override
+		public void movesCleard(GameState gameState) {
+			gs = gameState;
+		}
+		
+	}
 
 	Game g;
 	MrXPlayer mrX;
@@ -114,14 +237,18 @@ public class TheGameTest {
 
 	@Test
 	public final void testGetDetectives() {
+		
 		try {
 			g.getDetectives().set(2, d3);
 			fail("set works");
 		} catch (Exception e) { }
 		
-		g.getDetectives().add(d1);
-		g.getDetectives().add(0, d4);
-		g.getDetectives().remove(d4);
+		
+		DetectivePlayer dx = new DetectivePlayer();
+		DetectivePlayer dy = new DetectivePlayer();
+		g.getDetectives().add(dx);
+		g.getDetectives().add(0, dy);
+		g.getDetectives().remove(dy);
 		g.getDetectives().remove(4);
 		
 		assertEquals(4, g.getDetectives().size());
@@ -494,5 +621,132 @@ public class TheGameTest {
 		g.setCurrentPlayer(null);
 		assertEquals(null, g.getCurrentPlayer());
 	}
+	
+	@Test
+	public final void testStateListener() {
+		TestStateListener l = new TestStateListener();
+		g.addStateListener(l);
+		
+		g.setCurrentPlayer(null);
+		assertEquals(null, l.newP);
+		assertEquals(g, l.gs);
+		g.setCurrentPlayer(d3);
+		assertEquals(null, l.oldP);
+		assertEquals(d3, l.newP);
+		g.setCurrentPlayer(null);
+		assertEquals(d3, l.oldP);
+		assertEquals(null, l.newP);
+		
+		
+		g.setCurrentRoundNumber(2);
+		assertEquals(2, l.newR);
+		assertEquals(g, l.gs);
+		g.setCurrentRoundNumber(1);
+		assertEquals(2, l.oldR);
+		assertEquals(1, l.newR);
+		g.setCurrentRoundNumber(0);
+		assertEquals(1, l.oldR);
+		assertEquals(0, l.newR);
+	}
 
+	@Test
+	public final void testPlayerListener() {
+		TestPlayerListener l = new TestPlayerListener();
+		g.addPlayerListener(l);
+		
+		g.setMrX(null);
+		assertEquals(null, l.newX);
+		assertEquals(g, l.gs);
+		g.setMrX(mrX);
+		assertEquals(null, l.oldX);
+		assertEquals(mrX, l.newX);
+		g.setMrX(null);
+		assertEquals(mrX, l.oldX);
+		assertEquals(null, l.newX);
+		
+		DetectivePlayer dx = new DetectivePlayer();
+		DetectivePlayer dy = new DetectivePlayer();
+		g.getDetectives().add(dx);
+		assertEquals(g, l.gs);
+		assertEquals(4, l.i);
+		assertEquals(dx, l.d);
+		g.getDetectives().add(0, dy);
+		assertEquals(0, l.i);
+		assertEquals(dy, l.d);
+		g.getDetectives().remove(dy);
+		assertEquals(g, l.gs);
+		assertEquals(0, l.i);
+		assertEquals(dy, l.d);
+		g.getDetectives().remove(4);
+		assertEquals(4, l.i);
+		assertEquals(dx, l.d);
+		
+	}
+	
+	@Test
+	public final void testItemListener() {
+		TestItemListener l = new TestItemListener();
+		g.addItemListener(l);
+		
+		Set<Item> ims = new HashSet<>();
+		ims.add(new DoubleMoveCard());
+		
+		g.setItems(d1, ims);
+		assertEquals(g, l.gs);
+		assertEquals(1, l.newI.size());
+		assertEquals(d1, l.p);
+		ims = l.newI;
+		g.setItems(d1, null);
+		assertEquals(ims, l.oldI);
+		assertEquals(d1, l.p);
+		assertEquals(null, l.newI);
+
+		ims = new HashSet<>();
+		ims.add(new DoubleMoveCard());
+		ims.add(new TaxiTicket());
+		ims.add(new BusTicket());
+		g.setItems(mrX, ims);
+		
+		Item t = new UndergroundTicket();
+		g.getItems(mrX).add(t);
+		assertEquals(g, l.gs);
+		assertEquals(mrX, l.p);
+		assertEquals(t, l.i);
+				
+		g.getItems(mrX).remove(t);
+		assertEquals(g, l.gs);
+		assertEquals(mrX, l.p);
+		assertEquals(t, l.i);
+		
+		g.getItems(mrX).clear();
+		assertTrue(l.i != t); // weil t ja schon vorher geloescht wurde. is auch nur der beweis, dass der listener ueberhaupt bei clear aufgerufen wird.
+		
+	}
+	
+	@Test
+	public final void testMoveListener() {
+		TestMoveListener l = new TestMoveListener();
+		g.addMoveListener(l);
+		
+		for (int i = 0; i < 7; i++) {
+			l.gs = null;
+			g.getMoves().add(ms[i]);
+			assertEquals(g, l.gs);
+			assertEquals(ms[i], l.m);		
+		}
+		
+		for (int i = 6; i > 3; i--) {
+			l.gs = null;
+			g.getMoves().remove(GameState.LAST_MOVE);
+			assertEquals(g, l.gs);
+			assertEquals(ms[i], l.m);		
+		}
+		
+		l.gs = null;
+		g.getMoves().clear();
+		assertEquals(g, l.gs);
+		assertEquals(0, g.getMoves().size());
+		
+	}
+	
 }
