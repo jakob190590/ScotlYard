1d7dc1b8ce059cbca733d32fc9956c98e80fe5ba
Jakob Schöttl <jschoett@gmail.com>
2012-03-30 00:29:21 +0200

    More imp in util classes

--- ScotlYard/src/kj/scotlyard/game/util/GameStateExtension.java
+++ ScotlYard/src/kj/scotlyard/game/util/GameStateExtension.java
@@ -3,9 +3,7 @@ package kj.scotlyard.game.util;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.ListIterator;
-import java.util.Vector;
 
-import kj.scotlyard.game.control.GameStatus;
 import kj.scotlyard.game.model.GameState;
 import kj.scotlyard.game.model.Move;
 import kj.scotlyard.game.model.Player;
@@ -20,39 +18,60 @@ public class GameStateExtension {
 
 	public Move getMove(Player player, int roundNumber, int moveIndex) {
 		Move m = gameState.getMove(player, roundNumber, GameState.MoveAccessMode.ROUND_NUMBER);
-		return m.getMoves().get(moveIndex); // hier koennts index out of bound exception geben, illegal arg waere besser oder?
+		try {
+			return m.getMoves().get(moveIndex);
+		} catch (IndexOutOfBoundsException e) {
+			throw new IllegalArgumentException("The specified move index is not valid.", e);
+		}
 	}
 	
-	public List<Move> getMoves(int roundNumber) {
-		// TODO implement
-		return null;
+	public Move getLastMoveFlat(Player player) {
+		Move m = gameState.getLastMove(player);
+		List<Move> ms = m.getMoves();
+		if (!ms.isEmpty()) {
+			m = ms.get(ms.size() - 1);
+		}
+		return m;
 	}
 	
-	// flat (moveNumber) oder hierarchisch (roundNumber) ?
-	public ListIterator<Move> moveIterator(Player player, int moveNumber) { // oder roundNumber?
-		// TODO implement
-		return null;
-	}
-		
-	public ListIterator<Move> moveIterator(Player player) {
-		return moveIterator(player, GameState.INITIAL_);
-	}
-	
-	public List<Move> flattenMove(Move move) {
-		List<Move> result = new Vector<>();
-		result.add(move);
-		for (Move m : move.getMoves()) {
-			result.addAll(flattenMove(m));
+	public List<Move> getMoves(int roundNumber, boolean flat) {
+		List<Move> result = new LinkedList<>();
+		List<Move> all = (flat) ? getMovesFlat() : gameState.getMoves();
+		for (Move m : all) {
+			if (m.getRoundNumber() == roundNumber) {
+				result.add(m);
+			}
 		}
 		return result;
 	}
 	
-	public List<Move> getAllMoves() {
+	public ListIterator<Move> moveIterator(Player player, boolean flat, int moveRespRoundNumber) {
+		List<Move> all = (flat) ? getMovesFlat() : gameState.getMoves();
+		// TODO implement
+		return null;
+	}
+		
+	public ListIterator<Move> moveIterator(Player player, boolean flat) {
+		return moveIterator(player, flat, 0);
+	}
+	
+	public List<Move> flattenMove(Move move, boolean alsoReturnRootMove) {
+		List<Move> result = new LinkedList<>();
+		if (alsoReturnRootMove) {
+			result.add(move);
+		}
+		for (Move m : move.getMoves()) {
+			result.addAll(flattenMove(m, alsoReturnRootMove));
+		}
+		return result;
+	}
+	
+	public List<Move> getMovesFlat() {
 		List<Move> result = new LinkedList<>();
 		for (Move m : gameState.getMoves()) {
 			// auch das hier koennte man effizienter machen:
 			// if (m.getMoves().isEmpty()) result.add(m); else
-			result.addAll(flattenMove(m));
+			result.addAll(flattenMove(m, false));
 		}
 		return result;
 	}
--- ScotlYard/src/kj/scotlyard/game/util/MrXTracker.java
+++ ScotlYard/src/kj/scotlyard/game/util/MrXTracker.java
@@ -9,7 +9,9 @@ import kj.scotlyard.game.graph.GameGraph;
 import kj.scotlyard.game.graph.StationVertex;
 import kj.scotlyard.game.model.GameState;
 import kj.scotlyard.game.model.Move;
+import kj.scotlyard.game.model.Player;
 import kj.scotlyard.game.model.items.Ticket;
+import kj.scotlyard.game.rules.Rules;
 
 public class MrXTracker {
 
@@ -17,22 +19,37 @@ public class MrXTracker {
 	
 	private GameGraph gameGraph;
 	
+	private Rules rules;
+	
 	private GameStateExtension gameStateExtension;
 	
-	public MrXTracker(GameState gameState, GameGraph gameGraph) {
+	public MrXTracker(GameState gameState, GameGraph gameGraph, Rules rules) {
 		super();
 		this.gameState = gameState;
 		this.gameGraph = gameGraph;
 		gameStateExtension = new GameStateExtension(gameState);
+		this.rules = rules;
 	}
 
 	public Move getLastKnownMove() {
-		// TODO implement
+		Player mrX = gameState.getMrX();
+		Move lastMove = gameStateExtension.getLastMoveFlat(mrX);
+		
+		List<Integer> uncover = rules.getGameStateAccessPolicy().getMrXUncoverMoveNumbers();
+		ListIterator<Integer> it = uncover.listIterator(uncover.size());
+		while (it.hasPrevious()) {
+			int n = it.previous();
+			if (lastMove.getMoveNumber() >= n) {
+				return gameState.getMove(mrX, n, GameState.MoveAccessMode.MOVE_NUMBER);
+			}
+		}
+		
 		return null;
 	}
 	
 	public Set<StationVertex> getPossiblePositions() {
 		// ohne bewertung, denn das ist aufgabe der AI!
+		
 		// TODO implement
 		return null;
 	}
@@ -41,9 +58,13 @@ public class MrXTracker {
 		// ... Since last known Move, was sonst ...
 		
 		List<Ticket> list = new LinkedList<>();
-		ListIterator<Move> it = gameStateExtension.moveIterator(gameState.getMrX(), getLastKnownMove().getRoundNumber());
-		while (it.hasNext()) {
-			list.add((Ticket) it.next().getItem()); // hier koennts cast exception geben, bei corrupt game state
+		Move last = getLastKnownMove();
+		if (last != null) {
+			ListIterator<Move> it = gameStateExtension.moveIterator(gameState.getMrX(), true, last.getRoundNumber() + 1);
+			while (it.hasNext()) {
+				// hier koennts cast exception geben, bei corrupt game state
+				list.add((Ticket) it.next().getItem()); 
+			}
 		}
 		return list;
 	}
