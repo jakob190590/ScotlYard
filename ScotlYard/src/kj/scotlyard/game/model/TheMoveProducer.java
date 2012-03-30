915e6b73687815c4ab7b6e10c0414b968882da7b
Jakob Schöttl <jschoett@gmail.com>
2012-03-30 23:40:50 +0200

    Ganz ok soweit, nur die Erzeugung gefaellt mir noch nicht

--- ScotlYard/src/kj/scotlyard/game/model/TheMoveProducer.java
+++ ScotlYard/src/kj/scotlyard/game/model/TheMoveProducer.java
@@ -15,19 +15,30 @@ import kj.scotlyard.game.model.items.Ticket;
 // -> Deswegen was eigenes: Producer
 public class TheMoveProducer {
 	
-	private static List<Move> subMoves = new LinkedList<>();
+	private List<Move> subMoves = new LinkedList<>();
 
+	/**
+	 * Creates and returns a new instance of TheMoveProducer.
+	 * At least every thread should have it's own Producer.
+	 * Using the default constructer instead of this method
+	 * has the exactly same effect. 
+	 * @return a new instance to work with
+	 */
+	public static TheMoveProducer createInstance() {
+		return new TheMoveProducer(); 
+	}	
+	
 	
 	// Initial Move
 	
-	public static Move createInitialMove(Player player, int roundNumber, StationVertex station) {
+	public Move createInitialMove(Player player, int roundNumber, StationVertex station) {
 		
 		// roundNumber muss nicht INITIAL_ROUND_NUMBER sein! Player kann auch spaeter joinen.
 		return new DefaultMove(player, roundNumber, 
 				GameState.INITIAL_MOVE_NUMBER, Move.NO_MOVE_INDEX, station, null, null);
 	} 
 	
-	public static Move createInitialMove(Player player, StationVertex station) {
+	public Move createInitialMove(Player player, StationVertex station) {
 		
 		return createInitialMove(player, GameState.INITIAL_ROUND_NUMBER, station);
 	} 
@@ -35,7 +46,7 @@ public class TheMoveProducer {
 	
 	// Single Move
 	
-	public static Move createSingleMove(Player player, int roundNumber, int moveNumber, 
+	public Move createSingleMove(Player player, int roundNumber, int moveNumber, 
 			StationVertex station, ConnectionEdge connection, Ticket ticket) {
 		
 		return new DefaultMove(player, roundNumber, moveNumber, 
@@ -44,9 +55,12 @@ public class TheMoveProducer {
 	
 	
 	
-	// Multi Move (Builder interface)
+	// Multi Move (Builder interface)	
 	
-	public static void addSubMove(StationVertex station, 
+	// Because of this methods, there must be at least one instance per thread.
+	// With static methods instead, the multi move production would not be thread-safe. 
+	
+	public void addSubMove(StationVertex station, 
 			ConnectionEdge connection, Ticket ticket) {
 		
 		// Player and Numbers will be set later in createMultiMove
@@ -54,25 +68,29 @@ public class TheMoveProducer {
 				Move.NO_MOVE_INDEX, station, connection, ticket));
 	}
 	
-	public static void discardSubMoves() {
+	public void discardSubMoves() {
 		subMoves.clear();
 	}
 	
-	public static Move createMultiMove(Player player, int roundNumber, 
-			int firstMoveNumber, Item item, Move ...moves) {
+	public Move createMultiMove(Player player, int roundNumber, 
+			int firstMoveNumber, Item item) {
 		
 		Move m = new DefaultMove(player, roundNumber, Move.NO_MOVE_NUMBER, 
-				Move.NO_MOVE_INDEX, null, null, item, moves);
+				Move.NO_MOVE_INDEX, null, null, item);
+		
 		
 		int i = 0;
-		for (Move n : moves) {
+		for (Move n : subMoves) {
 			n.setPlayer(player);
 			n.setRoundNumber(roundNumber);
 			n.setMoveNumber(firstMoveNumber + i);
 			n.setMoveIndex(i++);
 		}
-		m.setStation(moves[moves.length - 1].getStation());
+		m.setStation(subMoves.get(subMoves.size() - 1).getStation());
 
+		m.getMoves().addAll(subMoves);
+		subMoves.clear();
+		
 		return m;
 	}
 }
