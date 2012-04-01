a280082f315b7aa403dc5886950c57e88e951868
Jakob Schöttl <jschoett@gmail.com>
2012-04-01 13:18:25 +0200

    Main algorithm finished; pending revision

--- ScotlYard/src/kj/scotlyard/game/rules/TheGameWinPolicy.java
+++ ScotlYard/src/kj/scotlyard/game/rules/TheGameWinPolicy.java
@@ -1,6 +1,7 @@
 package kj.scotlyard.game.rules;
 
 import java.util.List;
+import java.util.ListIterator;
 
 import kj.scotlyard.game.graph.GameGraph;
 import kj.scotlyard.game.model.DetectivePlayer;
@@ -22,16 +23,24 @@ public class TheGameWinPolicy implements GameWinPolicy {
 		
 		return false;
 	}
+	
+	private boolean canMove(DetectivePlayer detective) {
+		// fuer jede anliegende edge pruefen, ob ticket da is
+		// wenn ticket da is, pruefen, ob benachbarte station frei is (kein anderer detektiv)
+		
+		// TODO implement, korbi?
+		
+		return false;
+	}
 
 	@Override
 	public GameWin isGameWon(GameState gameState, GameGraph gameGraph) {
 		
 		Player mrX = gameState.getMrX();
-		Player lastDetective = gameState.getDetectives().get(gameState.getDetectives().size() - 1);
+		List<Move> allMoves = gameState.getMoves();
 		
 		// TODO zu spielbeginn is es gefaehrlich
 		Move mrXLastMove = gameState.getLastMove(mrX);
-		Move lastMove = gameState.getMoves().get(GameState.LAST_MOVE);
 		
 		
 		// Detectives win wenn 
@@ -62,17 +71,31 @@ public class TheGameWinPolicy implements GameWinPolicy {
 		}
 		
 		// mrX ertappt
-		// TODO ist no ned testreif
-		if (gameState.getCurrentPlayer() == mrX) {//lastMove.getPlayer() != mrX && lastMove.getStation() == mrXLastMove.getStation()) {
-			for (DetectivePlayer detective : gameState.getDetectives()) {
-				if (gameState.getLastMove(detective).getStation() == mrXLastMove.getStation()) {					
-					return GameWin.DETECTIVES_WIN;
-				}
+		// isGameWon wird ja nach jedem move abgefragt.
+		// solln wir also hier
+		// 1.) immer nur den letzten detective move testen, oder
+		// 2.) den jeweils letzten move aller detectives, die nach mrX dran waren?
+		// loesungsmoeglichkeit: hinten anfangen: dann laeufts im normalfall
+		// auf 1.) raus, is aber trotzdem sicher wie 2.)		
+		Move move;
+		ListIterator<Move> it = allMoves.listIterator(allMoves.size());
+		while (it.hasPrevious() && (move = it.previous()) != mrXLastMove) {
+			if (move.getStation() == mrXLastMove.getStation()) {
+				return GameWin.DETECTIVES_WIN;
 			}
 		}
 		
 		// detectives koennen nimmer ziehen
-				
+		boolean noDetectiveCanMove = true;
+		for (DetectivePlayer detective : gameState.getDetectives()) {
+			if (canMove(detective)) {
+				noDetectiveCanMove = false;
+				break;
+			}
+		}
+		if (noDetectiveCanMove) {
+			return GameWin.MRX_WINS;
+		}
 		
 		
 		return GameWin.NO;
