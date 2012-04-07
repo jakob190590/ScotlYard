ed603fdf62b1ca458e1ebef4176bb668e2adb63f
Jakob Schöttl <jschoett@gmail.com>
2012-04-07 21:12:33 +0200

    Graph imp added

--- ScotlYard/src/kj/scotlyard/game/graph/Station.java
+++ ScotlYard/src/kj/scotlyard/game/graph/Station.java
@@ -1,5 +1,23 @@
 package kj.scotlyard.game.graph;
 
-public class Station {
+import java.util.Set;
+
+public class Station implements StationVertex {
+	
+	private GameGraph graph;
+
+	public Station(GameGraph graph) {
+		this.graph = graph;
+	}
+
+	@Override
+	public Set<ConnectionEdge> getEdges() {		
+		return graph.edgesOf(this);
+	}
+
+	@Override
+	public Set<ConnectionEdge> getEdges(StationVertex otherVertex) {
+		return graph.getAllEdges(this, otherVertex);
+	}
 
 }
