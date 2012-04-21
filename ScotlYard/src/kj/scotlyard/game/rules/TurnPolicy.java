4cd0c7e137346fe998a83dba478d08c362bb3b29
Jakob Schöttl <jschoett@gmail.com>
2012-04-21 21:04:28 +0200

    Design changed in TurnPolicy rule

--- ScotlYard/src/kj/scotlyard/game/rules/TurnPolicy.java
+++ ScotlYard/src/kj/scotlyard/game/rules/TurnPolicy.java
@@ -2,12 +2,9 @@ package kj.scotlyard.game.rules;
 
 import kj.scotlyard.game.graph.GameGraph;
 import kj.scotlyard.game.model.GameState;
-import kj.scotlyard.game.model.Player;
 
 public interface TurnPolicy {
 
-	Player getNextPlayer(GameState gameState, GameGraph gameGraph);
-	
-	int getNextRoundNumber(GameState gameState, GameGraph gameGraph);
-	
+	Turn getNextTurn(GameState gameState, GameGraph gameGraph);
+
 }
