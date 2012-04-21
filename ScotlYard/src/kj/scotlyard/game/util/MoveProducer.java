f58e94fd795ca7eac1433c8795750618ca63bbe3
Jakob Schöttl <jschoett@gmail.com>
2012-04-22 00:04:34 +0200

    create Next Best Single Move method added to MoveProducer

--- ScotlYard/src/kj/scotlyard/game/util/MoveProducer.java
+++ ScotlYard/src/kj/scotlyard/game/util/MoveProducer.java
@@ -4,13 +4,17 @@ import java.util.LinkedList;
 import java.util.List;
 
 import kj.scotlyard.game.graph.ConnectionEdge;
+import kj.scotlyard.game.graph.GameGraph;
 import kj.scotlyard.game.graph.StationVertex;
 import kj.scotlyard.game.model.DefaultMove;
 import kj.scotlyard.game.model.GameState;
 import kj.scotlyard.game.model.Move;
 import kj.scotlyard.game.model.Player;
 import kj.scotlyard.game.model.item.DoubleMoveCard;
+import kj.scotlyard.game.model.item.Item;
 import kj.scotlyard.game.model.item.Ticket;
+import kj.scotlyard.game.rules.MovePolicy;
+import kj.scotlyard.game.rules.TheMovePolicy;
 
 // Zum Klassennamen:
 // -  Keine Factory im Sinne des Abstract Factory Pattern
@@ -95,5 +99,47 @@ public class MoveProducer {
 		subMoves.clear();
 		
 		return m;
+	}
+	
+	
+	
+	// Next Best Single Move
+	
+	public Move createNextBestSingleMove(GameState gameState, GameGraph gameGraph) {
+		
+		MovePolicy movePolicy = new TheMovePolicy();
+		
+		Move last = gameState.getMoves().get(GameState.LAST_MOVE);
+		Move m = new DefaultMove(gameState.getCurrentPlayer(), gameState.getCurrentRoundNumber(), 
+				last.getMoveNumber() + 1, Move.NO_MOVE_INDEX, null, null, null);
+		
+		StationVertex station = last.getStation();
+		for (ConnectionEdge e : station.getEdges()) {
+			boolean hasTicket = false;
+			for (Item i : gameState.getItems(last.getPlayer())) {
+				if (i instanceof Ticket && movePolicy.isTicketValidForConnection((Ticket) i, e)) {
+					m.setItem(i);
+					hasTicket = true;
+					break;
+				}
+				if (hasTicket) {
+					StationVertex oStation = e.getOther(station);
+					boolean vacant = true;
+					for (Player p : gameState.getPlayers()) {
+						if (p != last.getPlayer() && gameState.getLastMove(p).getStation() == oStation) {
+							vacant = false;
+							break;
+						}
+					}
+					if (vacant) {
+						m.setConnection(e);
+						m.setStation(oStation);
+						return m;
+					}
+				}
+			}
+		}
+		
+		throw new IllegalStateException("Current player cannot move.");
 	}
 }
