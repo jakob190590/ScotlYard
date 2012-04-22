b3011c263fb771dc95d240d2372d3d8b79ff1275
Jakob Schöttl <jschoett@gmail.com>
2012-04-22 14:29:58 +0200

    Tests for GameController finished

--- Board/test/kj/scotlyard/game/control/impl/InGameControllerTest.java
+++ Board/test/kj/scotlyard/game/control/impl/InGameControllerTest.java
@@ -3,6 +3,7 @@ package kj.scotlyard.game.control.impl;
 import static org.junit.Assert.*;
 
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.Set;
 
 import kj.scotlyard.board.BoardGraphLoader;
@@ -14,12 +15,17 @@ import kj.scotlyard.game.model.Game;
 import kj.scotlyard.game.model.GameState;
 import kj.scotlyard.game.model.Move;
 import kj.scotlyard.game.model.MrXPlayer;
+import kj.scotlyard.game.model.Player;
 import kj.scotlyard.game.model.TheGame;
+import kj.scotlyard.game.model.item.Item;
+import kj.scotlyard.game.model.item.Ticket;
 import kj.scotlyard.game.util.MoveProducer;
 import kj.scotlyard.game.rules.GameWin;
 import kj.scotlyard.game.rules.GameWinPolicy;
+import kj.scotlyard.game.rules.MovePolicy;
 import kj.scotlyard.game.rules.Rules;
 import kj.scotlyard.game.rules.TheGameWinPolicy;
+import kj.scotlyard.game.rules.TheMovePolicy;
 import kj.scotlyard.game.rules.TheRules;
 
 import org.junit.Before;
@@ -177,22 +183,72 @@ public class InGameControllerTest {
 		c.abort();
 		assertEquals(GameWin.NO, c.getWin());
 		assertEquals(GameStatus.NOT_IN_GAME, c.getStatus());
+		
+		c.getUndoManager().undo();
+		assertEquals(GameWin.NO, c.getWin());
+		assertEquals(GameStatus.IN_GAME, c.getStatus());
+		
+		c.getUndoManager().redo();
+		assertEquals(GameWin.NO, c.getWin());
+		assertEquals(GameStatus.NOT_IN_GAME, c.getStatus());
 	}
 
 	@Test
 	public final void testMove() {
-		GameWinPolicy winPolicy = new TheGameWinPolicy();
+		MovePolicy movePolicy = new TheMovePolicy();
+		GameWinPolicy winPolicy = new TheGameWinPolicy();		
 		
 		for (int i = 0; i < 100; i++) {
 			Move m = mp.createNextBestSingleMove(g, gg);
+			Set<Item> i1 = new HashSet<>(g.getItems(m.getPlayer()));
+			
+			GameState vorher = g.copy();
 			
 			c.move(m);
 			
-			assertEquals(m, g.getMoves().get(GameState.LAST_MOVE));
+			GameState nacher = g.copy();
 			
-			// TODO sicherstellen, dass tickets richtig weitergegeben wurden
+			assertEquals(m, g.getMoves().get(GameState.LAST_MOVE));
+
+			// Schauen, ob Ticket richtig weitergegeben wurde
+			Set<Item> i2 = new HashSet<>(g.getItems(m.getPlayer()));
+			i1.removeAll(i2);
+			assertEquals(1, i1.size());
+			Ticket t = (Ticket) i1.iterator().next();
+			assertTrue(movePolicy.isTicketValidForConnection(t, m.getConnection()));
+			if (m.getPlayer() != g.getMrX()) {
+				assertTrue(g.getItems(g.getMrX()).contains(t));
+			}
 			
 			GameWin win = winPolicy.isGameWon(g, gg); 
+			assertEquals(win, c.getWin());
+			if (win != GameWin.NO) {
+				assertEquals(GameStatus.NOT_IN_GAME, c.getStatus());
+			}	
+						
+			// Undo ------------
+			c.getUndoManager().undo();
+			assertEquals(vorher.getCurrentPlayer(), g.getCurrentPlayer());
+			assertEquals(vorher.getCurrentRoundNumber(), g.getCurrentRoundNumber());
+			assertEquals(vorher.getMoves(), g.getMoves());
+			assertEquals(vorher.getPlayers(), g.getPlayers());			
+			for (Player p : g.getPlayers()) {
+				assertEquals(vorher.getItems(p).size(), g.getItems(p).size());
+			}
+			assertEquals(GameStatus.IN_GAME, c.getStatus());
+			assertEquals(GameWin.NO, c.getWin());
+			
+
+			// Redo ------------
+			c.getUndoManager().redo();
+			assertEquals(nacher.getCurrentPlayer(), g.getCurrentPlayer());
+			assertEquals(nacher.getCurrentRoundNumber(), g.getCurrentRoundNumber());
+			assertEquals(nacher.getMoves(), g.getMoves());
+			assertEquals(nacher.getPlayers(), g.getPlayers());			
+			for (Player p : g.getPlayers()) {				
+				assertEquals(nacher.getItems(p), g.getItems(p));
+			}			
+			assertEquals(win, c.getWin());
 			if (win != GameWin.NO) {
 				System.out.println(win);
 				assertEquals(GameStatus.NOT_IN_GAME, c.getStatus());
