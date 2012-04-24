f9e85bbd3d3fcef1c9392c7ce5aa549527f16974
Jakob Schöttl <jschoett@gmail.com>
2012-04-24 22:30:32 +0200

    ItemDealer helper class added in utils
    Die ist nuetzlich fuer GUI/Controller: Fuer Experimente oder wenn Spieler
    nachtraeglich joinen bzw. Karten manuell ausgeteilt werden muessen.
    Ist ein Wrapper um Game, der nur die benoetigten Methoden und
    Erweiterungen zur Verfuegung
    stellt.

---
+++ ScotlYard/src/kj/scotlyard/game/util/ItemDealer.java
@@ -0,0 +1,94 @@
+package kj.scotlyard.game.util;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import kj.scotlyard.game.model.Game;
+import kj.scotlyard.game.model.Player;
+import kj.scotlyard.game.model.item.Item;
+
+public class ItemDealer {
+	
+	private Game game;
+
+	public ItemDealer(Game game) {
+		this.game = game;
+	}
+	
+	
+	
+	public Set<Item> getItems(Player player) {
+		return game.getItems(player);
+	}
+	
+	public void setItems(Player player, Set<Item> items) {
+		game.setItems(player, items);
+	}
+	
+	
+	
+	int countItems(Player player, Class<? extends Item> itemType) {
+		int n = 0;
+		for (Item i : game.getItems(player))
+			if (i.getClass() == itemType)
+				n++;
+
+		return n;
+	}
+	
+	boolean addItems(Player player, int count, Class<? extends Item> itemType) {
+		try {
+			Set<Item> set = new HashSet<>();
+			for (int i = 0; i < count; i++) {
+				set.add(itemType.newInstance());
+			}
+			
+			game.getItems(player).addAll(set);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+	
+	int passItems(Player fromPlayer, Player toPlayer, int count, Class<? extends Item> itemType) {
+		int n = 0;
+		for (Item i : game.getItems(fromPlayer))		
+			if (n < count) {
+				if (i.getClass() == itemType) {
+					game.getItems(fromPlayer).remove(i);
+					game.getItems(toPlayer).add(i);
+					n++;
+				}
+			} else {
+				break;
+			}
+
+		return n;
+	}
+	
+	int removeItems(Player player, int count, Class<? extends Item> itemType) {
+		int n = 0;
+		for (Item i : game.getItems(player))		
+			if (n < count) {
+				if (i.getClass() == itemType) {
+					game.getItems(player).remove(i);
+					n++;
+				}
+			} else {
+				break;
+			}
+
+		return n;
+	}
+	
+	int removeAllItems(Player player, Class<? extends Item> itemType) {
+		int n = 0;
+		for (Item i : game.getItems(player))		
+			if (i.getClass() == itemType) {
+				game.getItems(player).remove(i);
+				n++;
+			}
+		return n;
+	}
+
+}
