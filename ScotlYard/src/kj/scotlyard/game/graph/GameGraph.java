da06a7d341d88c00fc7af29bad0eb9f71bdf004f
Jakob Schöttl <jschoett@gmail.com>
2012-05-09 23:30:02 +0200

    GameGraph angepasst (und verkompliziert)

--- ScotlYard/src/kj/scotlyard/game/graph/GameGraph.java
+++ ScotlYard/src/kj/scotlyard/game/graph/GameGraph.java
@@ -1,26 +1,64 @@
 package kj.scotlyard.game.graph;
 
+import java.util.Collections;
 import java.util.Set;
 
+import org.apache.commons.attributes.DefaultSealable;
+import org.apache.commons.attributes.Sealable;
 import org.jgrapht.UndirectedGraph;
+import org.jgrapht.graph.UnmodifiableUndirectedGraph;
 
-public class GameGraph {
+/**
+ * This class is a wrapper around the (game) graph:
+ * Graph and initial stations.
+ * 
+ * Beide Felder muessen gesetzt werden, aber das geht
+ * leider nicht durch den Konstruktor, sondern nur mit
+ * den Settern. Wenn der GameGraph fertig ist muss er
+ * mit der Methode <tt>seal</tt> "sealed & released"
+ * werden. Fuer Hintergruende: Entwicklerhandbuch.
+ * 
+ * @author jakob190590
+ *
+ */
+public class GameGraph extends DefaultSealable implements Sealable {
 
 	private UndirectedGraph<StationVertex, ConnectionEdge> graph;
 	
 	private Set<StationVertex> initialStations;
 	
-	public GameGraph(UndirectedGraph<StationVertex, ConnectionEdge> graph, Set<StationVertex> initialStations) {
+	public GameGraph() { }
+
+	/**
+	 * Seals and releases the GameGraph. Both graph and initial stations
+	 * must be set. After this, both will be unmodifiable (in every respect).
+	 * @throws NullPointerException if graph or initial stations is <tt>null</tt>.
+	 */
+	@Override
+	public void seal() throws NullPointerException {
+		// To seal and release this GameGraph		
 		if (graph == null || initialStations == null) {
-			throw new NullPointerException("Graph and Initial Stations must not be null.");
+			throw new NullPointerException("GameGraph cannot be sealed and " +
+					"released: Graph and Initial Stations must not be null.");
 		}
-		this.graph = graph;
-		this.initialStations = initialStations;
-		// TODO wer sorgt dafuer, dass graph und initial stations unmodifiable sind??
+		
+		graph = new UnmodifiableUndirectedGraph<>(graph);
+		initialStations = Collections.unmodifiableSet(initialStations);
+		super.seal();
 	}
 	
+	public void setGraph(UndirectedGraph<StationVertex, ConnectionEdge> graph) {
+		checkSealed();
+		this.graph = graph;
+	}
+
 	public UndirectedGraph<StationVertex, ConnectionEdge> getGraph() {
 		return graph;
+	}
+	
+	public void setInitialStations(Set<StationVertex> initialStations) {
+		checkSealed();
+		this.initialStations = initialStations;
 	}
 	
 	public Set<StationVertex> getInitialStations() {
