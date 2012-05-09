10b16078eed0eb23d312a33f5d346dae7a98697f
Jakob Schöttl <jschoett@gmail.com>
2012-05-09 20:56:07 +0200

    Getter for Initial Stations added to GameGraph

--- ScotlYard/src/kj/scotlyard/game/graph/GameGraph.java
+++ ScotlYard/src/kj/scotlyard/game/graph/GameGraph.java
@@ -1,20 +1,30 @@
 package kj.scotlyard.game.graph;
 
+import java.util.Set;
+
 import org.jgrapht.UndirectedGraph;
 
 public class GameGraph {
 
 	private UndirectedGraph<StationVertex, ConnectionEdge> graph;
 	
-	public GameGraph(UndirectedGraph<StationVertex, ConnectionEdge> graph) {
-		if (graph == null) {
-			throw new NullPointerException("Graph must not be null.");
+	private Set<StationVertex> initialStations;
+	
+	public GameGraph(UndirectedGraph<StationVertex, ConnectionEdge> graph, Set<StationVertex> initialStations) {
+		if (graph == null || initialStations == null) {
+			throw new NullPointerException("Graph and Initial Stations must not be null.");
 		}
 		this.graph = graph;
+		this.initialStations = initialStations;
+		// TODO wer sorgt dafuer, dass graph und initial stations unmodifiable sind??
 	}
 	
 	public UndirectedGraph<StationVertex, ConnectionEdge> getGraph() {
 		return graph;
+	}
+	
+	public Set<StationVertex> getInitialStations() {
+		return initialStations;
 	}
 	
 }
