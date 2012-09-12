30ea49c988a27f704e5767af41a57f592790b597
Jakob Schöttl <jschoett@gmail.com>
2012-09-12 22:24:19 +0200

    0xF6 ersetzt durch UTF-8 ö

--- Board/src/kj/scotlyard/board/Piece.java
+++ Board/src/kj/scotlyard/board/Piece.java
@@ -1,6 +1,6 @@
 /*
  * ScotlYard -- A software implementation of the Scotland Yard board game
- * Copyright (C) 2012  Jakob Sch�ttl
+ * Copyright (C) 2012  Jakob Schöttl
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
