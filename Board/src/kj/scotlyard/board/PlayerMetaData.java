a9395f6782c3a4bc24008a0df48586b433fc42b7
Jakob Schöttl <jschoett@gmail.com>
2012-09-26 18:13:46 +0200

    PlayerInfo renamed to PlayerMetaData

--- Board/src/kj/scotlyard/board/PlayerMetaData.java
+++ Board/src/kj/scotlyard/board/PlayerMetaData.java
@@ -2,13 +2,13 @@ package kj.scotlyard.board;
 
 import java.awt.Color;
 
-public class PlayerInfo {
+public class PlayerMetaData {
 
 	private String name;
 
 	private Color color;
 	
-	public PlayerInfo(String name, Color color) {
+	public PlayerMetaData(String name, Color color) {
 		this.name = name;
 		this.color = color;
 	}
