d079bdd819ce0885a4f5b687efd9715f748d2335
Jakob Schöttl <jschoett@gmail.com>
2012-09-29 19:51:19 +0200

    Test Suites added

---
+++ Board/test/kj/scotlyard/board/AllTests.java
@@ -0,0 +1,11 @@
+package kj.scotlyard.board;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ ActionToolsTest.class })
+public class AllTests {
+
+}
