0aa79fa17f699c4dce42e89bf7fb3a2b8ce9d8df
Jakob Schöttl <jschoett@gmail.com>
2012-09-29 09:36:54 +0200

    Not tested yet: setNameAndMnemonic for an Action

--- Board/src/kj/scotlyard/board/ActionTools.java
+++ Board/src/kj/scotlyard/board/ActionTools.java
@@ -1,6 +1,7 @@
 package kj.scotlyard.board;
 
 import javax.swing.Action;
+import javax.swing.KeyStroke;
 
 public abstract class ActionTools {
 	
@@ -10,6 +11,33 @@ public abstract class ActionTools {
 	
 	public static boolean isSelected(Action action) {
 		return (boolean) action.getValue(Action.SELECTED_KEY);
+	}
+	
+	/**
+	 * Set the <code>NAME</code>, <code>MNEMONIC_KEY</code> and
+	 * - if necessary - <code>DISPLAYED_MNEMONIC_INDEX_KEY</code>
+	 * for an Action. If the name contains one or more ampersands
+	 * (&), the letter directly following the first ampersand
+	 * will be the mnemonic key. The first ampersand in name will
+	 * be removed.
+	 * @param action
+	 * @param name
+	 */
+	public static void setNameAndMnemonic(Action action, String name) {
+		// Erstes & finden, Index merken und entfernen
+		int index = name.indexOf('&');
+		if (index >= 0) {
+			StringBuffer s = new StringBuffer(name);
+			s.deleteCharAt(index);
+			char c = s.charAt(index); // mnemonic char
+			action.putValue(Action.NAME, s);
+			action.putValue(Action.MNEMONIC_KEY, KeyStroke.getKeyStroke(c).getKeyCode());
+			if (name.indexOf(c) < index)
+				action.putValue(Action.DISPLAYED_MNEMONIC_INDEX_KEY, index);
+			
+		} else {
+			action.putValue(Action.NAME, name);
+		}
 	}
 
 }
