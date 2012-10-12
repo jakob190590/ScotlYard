a15e9e74d2945e75c1001bb1a7c4685d6993ea05
Jakob Schöttl <jschoett@gmail.com>
2012-10-12 22:34:03 +0200

    Rating modules added (raw)

---
+++ ScotlYard/src/kj/scotlyard/game/ai/mrx/modules/RatingModule.java
@@ -0,0 +1,34 @@
+/*
+ * ScotlYard -- A software implementation of the Scotland Yard board game
+ * Copyright (C) 2012  Jakob Schöttl, Korbinian Eckstein
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package kj.scotlyard.game.ai.mrx.modules;
+
+import java.util.Map;
+import java.util.Set;
+
+import kj.scotlyard.game.ai.mrx.Alternative;
+import kj.scotlyard.game.ai.mrx.Rating;
+import kj.scotlyard.game.graph.GameGraph;
+import kj.scotlyard.game.model.GameState;
+import kj.scotlyard.game.model.Player;
+
+public interface RatingModule {
+
+	Map<Alternative, Rating> rate(GameState gameState, GameGraph gameGraph, Player player, Set<Alternative> alternatives);
+	
+}
