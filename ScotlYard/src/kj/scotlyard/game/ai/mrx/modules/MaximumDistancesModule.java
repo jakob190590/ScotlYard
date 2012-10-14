1bbe25fb649c239a56191243a89db2f53ebf2819
Jakob Schöttl <jschoett@gmail.com>
2012-10-14 11:46:49 +0200

    Simple impl for max dist. rating module; without BFS

--- ScotlYard/src/kj/scotlyard/game/ai/mrx/modules/MaximumDistancesModule.java
+++ ScotlYard/src/kj/scotlyard/game/ai/mrx/modules/MaximumDistancesModule.java
@@ -18,22 +18,59 @@
 
 package kj.scotlyard.game.ai.mrx.modules;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import kj.scotlyard.game.ai.mrx.Alternative;
 import kj.scotlyard.game.ai.mrx.Rating;
+import kj.scotlyard.game.graph.ConnectionEdge;
 import kj.scotlyard.game.graph.GameGraph;
+import kj.scotlyard.game.graph.StationVertex;
+import kj.scotlyard.game.graph.connection.FerryConnection;
 import kj.scotlyard.game.model.GameState;
 import kj.scotlyard.game.model.Player;
+import kj.scotlyard.game.util.GameStateExtension;
+
+import org.jgrapht.alg.BellmanFordShortestPath;
 
 public class MaximumDistancesModule implements RatingModule {
 
 	@Override
 	public Map<Alternative, Rating> rate(GameState gameState,
 			GameGraph gameGraph, Player player, Set<Alternative> alternatives) {
-		// TODO Auto-generated method stub
-		return null;
+		Map<Alternative, Rating> result = new HashMap<>();
+		for (Alternative a : alternatives) {
+			StationVertex v = (a.isDoubleMove()) ? a.getVertex2() : a.getVertex1();
+			int distances[] = new int[gameState.getDetectives().size()];
+			int i = 0;
+			for (StationVertex vd : GameStateExtension.getDetectivePositions(gameState)) {
+				// TODO korbi, hier brauchen wir dann deine breiten suche, die die ferry conns weglaesst!
+				List<ConnectionEdge> path = BellmanFordShortestPath.findPathBetween(gameGraph.getGraph(), v, vd);
+				
+				// workaround wegen ferry conns:
+				
+				// zaehlen, wie viele ferry conns der pfad enthaelt
+				int nFerryConnections = 0;
+				for (ConnectionEdge e : path) {
+					if (e instanceof FerryConnection) {
+						nFerryConnections++;
+					}
+				}
+				// abschaetzung: distanz ist im schnitt 4 mal (2 x 3 mal, 1 x 5 mal) so lang auf normalem wege
+				distances[i] = path.size() + nFerryConnections * 3;
+				i++;
+			}
+			
+			double rating = 0;
+			for (int d : distances) {
+				rating += 1. / (d * d);
+			}
+			rating /= distances.length;
+			result.put(a, new Rating(1 - rating));
+		}
+		return result;
 	}
 
 }
