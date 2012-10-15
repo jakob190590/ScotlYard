1dd1564f0cc827c5f8f31e6c978448ffedb6af0d
Jakob Schöttl <jschoett@gmail.com>
2012-10-16 00:12:12 +0200

    Impl complete -- test pending

--- Board/src/kj/scotlyard/board/util/ListListModel.java
+++ Board/src/kj/scotlyard/board/util/ListListModel.java
@@ -7,14 +7,50 @@ import java.util.List;
 import java.util.Set;
 
 import javax.swing.ListModel;
+import javax.swing.event.ListDataEvent;
 import javax.swing.event.ListDataListener;
 
 public class ListListModel<E> extends AbstractList<E> implements ListModel<E> {
 	
-	List<E> list = new LinkedList<>();
+	private List<E> list = new LinkedList<>();
 	
-	Set<ListDataListener> listeners = new HashSet<>();
+	private Set<ListDataListener> listeners = new HashSet<>();
+	
+	private ListDataListener listDataEventDispatcher = new ListDataListener() {
+		@Override
+		public void intervalRemoved(ListDataEvent e) {
+			for (ListDataListener l : listeners) {
+				l.intervalRemoved(e);
+			}
+		}
+		@Override
+		public void intervalAdded(ListDataEvent e) {
+			for (ListDataListener l : listeners) {
+				l.intervalAdded(e);
+			}
+		}
+		@Override
+		public void contentsChanged(ListDataEvent e) {
+			for (ListDataListener l : listeners) {
+				l.contentsChanged(e);
+			}
+		}
+	};
 
+	private void dispatchListDataEvent(ListDataEvent e) {
+		switch (e.getType()) {
+		case ListDataEvent.CONTENTS_CHANGED:
+			listDataEventDispatcher.contentsChanged(e);
+			break;
+		case ListDataEvent.INTERVAL_ADDED:
+			listDataEventDispatcher.intervalAdded(e);
+			break;
+		case ListDataEvent.INTERVAL_REMOVED:
+			listDataEventDispatcher.intervalRemoved(e);
+			break;
+		}
+	}
+	
 	@Override
 	public void addListDataListener(ListDataListener l) {
 		listeners.add(l);
@@ -24,7 +60,10 @@ public class ListListModel<E> extends AbstractList<E> implements ListModel<E> {
 	public void removeListDataListener(ListDataListener l) {
 		listeners.remove(l);
 	}
+	
 
+	// Read-only
+	
 	@Override
 	public E get(int index) {
 		return list.get(index);
@@ -46,25 +85,28 @@ public class ListListModel<E> extends AbstractList<E> implements ListModel<E> {
 	}
 	
 	
-	// Impl for a modifyable List:
+	// Impl for a modifyable List
 	
 	@Override
 	public E set(int index, E element) {
 		E r = list.set(index, element);
-		// TODO inform listeners
+		dispatchListDataEvent(new ListDataEvent(this,
+				ListDataEvent.CONTENTS_CHANGED, index, index));
 		return r;
 	}
 
 	@Override
 	public void add(int index, E element) {
 		list.add(index, element);
-		// TODO inform listeners
+		dispatchListDataEvent(new ListDataEvent(this,
+				ListDataEvent.INTERVAL_ADDED, index, index));
 	}
 	
 	@Override
 	public E remove(int index) {
 		E r = list.remove(index);
-		// TODO inform listeners
+		dispatchListDataEvent(new ListDataEvent(this,
+				ListDataEvent.INTERVAL_REMOVED, index, index));
 		return r;
 	}
 }
